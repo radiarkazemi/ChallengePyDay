021e5588e660083fc621561e545e0f152c5ea13a
Mohammad kazemi <radiarkazemi@gmail.com>
2023-10-14 22:07:31 +0330

    Adding Day-2 Challenges - Level: Basic

---
+++ python_basic/find_the_runner-up_score.py
@@ -0,0 +1,22 @@
+"""
+Task:
+Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+
+Input Format:
+The first line contains n. The second line contains an array a of n integers each separated by a space.
+
+Output Format:
+Print the runner-up score.
+"""
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = map(int, input().split())
+
+    unique_scores = list(set(arr))
+
+    sorted_scores = sorted(unique_scores, reverse=True)
+
+    runner_up_score = sorted_scores[1]
+
+    print(runner_up_score)
